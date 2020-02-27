b6d8deb4b75bb7df51a1792080830ed65276d994
keneskhanovm <42913895+keneskhanovm@users.noreply.github.com>
2020-02-27 20:23:16 +0600

    Add files via upload

---
+++ my-app/src/app/products.ts
@@ -0,0 +1,83 @@
+export const products = [
+  {
+    url: 'https://www.aliexpress.com/item/4000020314317.html?spm=a2g0o.productlist.0.0.320c6378vNzF3c',
+    image: '../assets/img/1.jpg',
+    name: 'Floveme Bluetooth Headphones 5.0',
+    price: 13.94,
+    description: 'Stereo Wireless Earphone Mini Bluetooth Headset With Mic Charging Box For Xiaomi mi Phone',
+    rating: 4.8
+  },
+  {
+    url: 'https://www.aliexpress.com/item/4000020314317.html?spm=a2g0o.productlist.0.0.320c6378vNzF3c',
+    image: '../assets/img/1.jpg',
+    name: 'Floveme Bluetooth Headphones 5.0',
+    price: 13.94,
+    description: 'Stereo Wireless Earphone Mini Bluetooth Headset With Mic Charging Box For Xiaomi mi Phone',
+    rating: 4.8
+  },
+  {
+    url: 'https://www.aliexpress.com/item/4000020314317.html?spm=a2g0o.productlist.0.0.320c6378vNzF3c',
+    image: '../assets/img/1.jpg',
+    name: 'Floveme Bluetooth Headphones 5.0',
+    price: 13.94,
+    description: 'Stereo Wireless Earphone Mini Bluetooth Headset With Mic Charging Box For Xiaomi mi Phone',
+    rating: 4.8
+  },
+  {
+    image: '../assets/img/2.jpg',
+    name: 'FLOVEME Cloth Case',
+    price: 2.74,
+    description: 'High Quality Soft Silicone Cover',
+    rating: 4.9
+  },
+  {
+    image: '../assets/img/3.jpg',
+    name: 'FLOVEME 5V/2A Wireless Charger',
+    price: 9.99,
+    description: 'Wireless Charging Dock For iPhone X 8 8 Plus Pad',
+    rating: 4.4
+  },
+  {
+    image: '../assets/img/4.jfif',
+    name: 'iPhone 11',
+    price: 1.044,
+    description: 'A transformative triple‑camera system. An unprecedented leap in battery life.',
+    rating: 4.8
+  },
+  {
+    url: 'https://www.aliexpress.com/item/4000020314317.html?spm=a2g0o.productlist.0.0.320c6378vNzF3c',
+    image: 'https://ae01.alicdn.com/kf/H5fda57ef3535444d86e2525e5e1feaa35/Original-New-Arrival-NIKE-KYRIE-FLYTRAP-EP-Men-s-Basketball-Shoes-Sneakers.jpg',
+    name: 'NIKE KYRIE FLYTRAP EP',
+    price: 157.62,
+    description: 'Original New Arrival Mens Basketball Shoes Sneakers',
+    rating: 4.8
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/HTB1Lxv5MSzqK1RjSZFpq6ykSXXaa/New-Trend-Adults-Men-s-Outdoor-Soccer-Cleats-Shoes-High-Top-TF-FG-Football-Boots-Training.jpg',
+    name: 'Football Boots',
+    price: 11.90,
+    description: 'New Trend Adults Mens Outdoor Soccer Cleats Shoes High Top TF/FG Training Sports Sneakers Shoes Plus Size 33-45',
+    rating: 4.9
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/Hc79680c14bde4af089f6254505e29ccaJ/LION-SCREAM-Hiking-Shoes-Summer-Men-Quick-Dry-Water-Shoes-Mesh-Beach-Outdoor-Hiking-Sandals-Trekking.jpg',
+    name: 'LION SCREAM Hiking Shoes',
+    price: 20.68,
+    description: 'Summer Men Quick Dry Water Shoes Mesh Beach Outdoor Hiking Sandals Trekking Shoes Trail Footwear 39-50',
+    rating: 4.4
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/H2d9d8ca08d47456ead5efc9b66940cf3T/2018-Adult-Thermal-Warm-Thicken-Figure-Skating-Ice-Skates-Shoes-With-Ice-Blade-PVC-Waterproof-White.jpg',
+    name: 'Figure Skating Ice Skates Shoes',
+    price: 68.03,
+    description: '2018 Adult Thermal Warm Thicken With Ice Blade PVC Waterproof White Black',
+    rating: 5.0
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
