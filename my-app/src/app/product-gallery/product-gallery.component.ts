a225a337970628f2aebaf8957560abed25a9f7eb
keneskhanovm <42913895+keneskhanovm@users.noreply.github.com>
2020-02-27 20:31:06 +0600

    Add files via upload

---
+++ my-app/src/app/product-gallery/product-gallery.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-gallery',
+  templateUrl: './product-gallery.component.html',
+  styleUrls: ['./product-gallery.component.css']
+})
+export class ProductGalleryComponent implements OnInit {
+  products = products;
+  constructor(
+    // private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
